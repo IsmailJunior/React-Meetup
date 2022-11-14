3de6ef83830c257b085cf2498340f8ec9355baa7
Merced <77894409+ismailMerced@users.noreply.github.com>
2022-11-14 13:52:48 +0300

    connection firebase backend and auth

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -1,26 +1,66 @@
 /** @format */
 
-import { useState } from "react";
+import { useState, useRef } from "react";
 import classes from "./AuthForm.module.css";
 
 export default function AuthForm() {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
+  const api = "AIzaSyC20c5tIyNJc1pSe5lZwM4A6sEq6BfwGbQ";
 
-  const switchAuthModeHandler = () => {
+  function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
-  };
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    if (isLogin) {
+    } else {
+      fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${api}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+        }
+      ).then((res) => {
+        if (res.ok) {
+          return console.log(res);
+        } else {
+          return res.json().then((data) => {
+            console.log(data);
+          });
+        }
+      });
+    }
+  }
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required />
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? "Login" : "Create Account"}</button>
